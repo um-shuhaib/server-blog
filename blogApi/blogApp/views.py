d296fee555f30196a102fc5d602827613af1c0b7
um-shuhaib <shuhaib5560@gmail.com>
2025-11-22 12:11:18 +0530

    follower adding added

--- blogApi/blogApp/views.py
+++ blogApi/blogApp/views.py
@@ -5,7 +5,7 @@ from blogApp.serialiser import UserSerialiser,ProfileSerialiser
 from rest_framework.response import Response
 from blogApp.models import ProfileModel 
 from rest_framework import authentication,permissions
-
+from rest_framework.decorators import action
 
 # Create your views here.
 class UserView(ModelViewSet):
@@ -28,6 +28,13 @@ class ProfileView(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         return super().perform_create(serializer)
+    
+    @action(methods=["POST"],detail=True)
+    def add_followers(self,request,*args,**kwargs):
+        profile_to_follow=ProfileModel.objects.get(id=kwargs.get("pk"))
+        user=request.user
+        profile_to_follow.followers.add(user)
+        return Response({"msg":"followed"})
 
 
 
